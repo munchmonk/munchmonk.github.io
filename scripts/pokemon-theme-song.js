cbf21eb61c40c449dfb1aff82002eb085f0f0247
jackie saltalepre <pirrulez@gmail.com>
2020-07-14 10:38:14 +0100

    responsive image

---
+++ scripts/pokemon-theme-song.js
@@ -0,0 +1,4 @@
+secondSongUrl = 'https://www.youtube.com/embed/Qnn9cQagoug';
+secondIframe = document.getElementById('second-song');
+secondIframe.setAttribute('src', secondSongUrl);
+secondIframe.setAttribute('sandbox');
